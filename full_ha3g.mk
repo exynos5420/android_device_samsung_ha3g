b6af599252d6f281aa2127a5daf40eca906f670c
Ivan <ivan.flashka@gmail.com>
2015-01-05 18:37:29 +0300

    IMPR: correct device name properties

--- full_ha3g.mk
+++ full_ha3g.mk
@@ -4,8 +4,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/ha3g/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_ha3gxx
-PRODUCT_DEVICE := ha3gxx
+PRODUCT_NAME := full_ha3g
+PRODUCT_DEVICE := ha3g
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := ha3g
+PRODUCT_MODEL := SM-N900
