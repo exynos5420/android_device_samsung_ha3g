21c75876a5d6a983a4a7308689df2666677517e2
Ivan <ivan.flashka@gmail.com>
2015-01-06 17:40:46 +0300

    TASK: use custom wifi-conf files (initially from stock)

--- device.mk
+++ device.mk
@@ -269,6 +269,11 @@ PRODUCT_PACKAGES += \
     Torch
 
 # Wifi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+
 PRODUCT_PACKAGES += \
     libnetcmdiface \
     macloader \
