cc4e5373208669ef9757c32ba49299368760b2cb
Ivan <ivan.flashka@gmail.com>
2015-01-06 19:04:04 +0300

    ha3g: call correct memory profile

--- device.mk
+++ device.mk
@@ -287,6 +287,6 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
 # call hwui memory config
-# $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/ha3g/ha3g-vendor.mk)
