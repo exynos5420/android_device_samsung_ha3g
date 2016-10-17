0c975fe86ca128f3efdbc2dd7fe7dab70803a1bf
tincho5588 <tincho5588@gmail.com>
2016-10-17 01:32:06 -0300

    ha3g: Changed audio_effects.conf destination.
    Previous path would make the device bootloop.
    
    Change-Id: I1cc883258046505f81a671c4312f5d9e96e17f42

--- device.mk
+++ device.mk
@@ -34,7 +34,7 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/ysound.xml:system/etc/ysound.xml
