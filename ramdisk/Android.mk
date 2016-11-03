4f0f5e985a93802c596d7292f0d0902822d7de8c
Sean Hoyt <seanhoyt963@gmail.com>
2016-11-02 20:05:20 -0500

    Use exynos5420-common
    Change-Id: Icd858b972a49cb1112f8eff017fca35bfda0abbc

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -1,44 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.universal5420
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := fstab.universal5420
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.universal5420.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.universal5420.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.universal5420.usb.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.universal5420.usb.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.universal5420.wifi.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.universal5420.wifi.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE        := init.samsung.rc
+LOCAL_MODULE        := init.target.rc
 LOCAL_MODULE_TAGS   := optional eng
 LOCAL_MODULE_CLASS  := ETC
-LOCAL_SRC_FILES     := init.samsung.rc
+LOCAL_SRC_FILES     := init.target.rc
 LOCAL_MODULE_PATH   := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
