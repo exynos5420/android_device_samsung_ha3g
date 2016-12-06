2538ca2d40434ba87e83640583f5cce852635ad8
tincho5588 <tincho5588@gmail.com>
2016-12-06 20:52:39 -0300

    Convert to AICP
    Change-Id: I212a17d1d3abc66a5af0051bf81aa3e1d5d8acd4

--- aicp.mk
+++ aicp.mk
@@ -1,15 +1,17 @@
 # Inherit device configuration
 $(call inherit-product, device/samsung/ha3g/full_ha3g.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
+
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_ha3g
+PRODUCT_NAME := aicp_ha3g
 PRODUCT_DEVICE := ha3g
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-N900 \
     PRODUCT_NAME=ha3g \
     PRODUCT_DEVICE=ha3g \
-    TARGET_DEVICE=ha3g \
+    TARGET_DEVICE=ha3g 
