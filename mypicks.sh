60be9ac54160ec0fa7b453f3a8b2e9d2ea90dcaf
Martin Bouchet <tincho5588@gmail.com>
2017-02-08 18:44:38 -0300

    ha3g: Add a reference to the commits I pick to build
    patches have been replaced by cherry-picked commits also
    
    Change-Id: I9a6ca8c58addb92db117b20c33f41f1e754bc0b2

---
+++ mypicks.sh
@@ -0,0 +1,20 @@
+# I will list here the open commits I pick to build
+# From the LineageOS gerrit:
+repopick 166274 166331 166332
+
+# From the exynos5420 gerrit
+repopick 2836 2837 2851 -g https://review.exynos5420.com/
+
+# For Substratum Support
+repopick -t OMS7 -g https://review.lineageos.org
+
+# From my github
+cd system/core
+git fetch https://github.com/tincho5588/android_system_core && git cherry-pick 71487616edf3f78f317ce013a159eede6d2f072f
+croot
+cd frameworks/av
+git fetch https://github.com/tincho5588/android_frameworks_av && git cherry-pick c7b461fba377b95903c9d3f76be0f5011766ba8c
+croot
+cd packages/apps/Dialer
+git fetch https://github.com/tincho5588/android_packages_apps_Dialer.git && git cherry-pick a16575f9fd5bbd09d933bb36d33618880471e861
+croot
