2b1bc3c97d679497fbcc2df971e5e092e584085a
Martin Bouchet <tincho5588@gmail.com>
2017-02-08 00:12:04 -0300

    RIL: define modem properly
    ha3g has xmm6360, not xmm6260
    
    Change-Id: Ibc116b7dfd08e5192797025f340fd277f45ff449

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@ TARGET_SOC := exynos5420
 # Radio
 BOARD_PROVIDES_LIBRIL := true
 # hardware/samsung/ril
-BOARD_MODEM_TYPE := xmm6260
+BOARD_MODEM_TYPE := xmm6360
 # we need define it (because audio.primary.universal5420.so requires it)
 COMMON_GLOBAL_CFLAGS += -DSEC_PRODUCT_FEATURE_RIL_CALL_DUALMODE_CDMAGSM
 # RIL.java overwrite
