76309c9b1e41b3bd535babc9bebe8cc158f4c2ce
Martin Bouchet <tincho5588@gmail.com>
2017-02-22 01:37:15 -0300

    ha3g: Switch to the new audio HAL in hardware/samsung
    Change-Id: I230f6433b6d891fa3bead301af93501f9963c384

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,8 +24,6 @@ TARGET_SOC := exynos5420
 BOARD_PROVIDES_LIBRIL := true
 # hardware/samsung/ril
 BOARD_MODEM_TYPE := xmm6360
-# we need define it (because audio.primary.universal5420.so requires it)
-BOARD_GLOBAL_CFLAGS += -DSEC_PRODUCT_FEATURE_RIL_CALL_DUALMODE_CDMAGSM
 # RIL.java overwrite
 BOARD_RIL_CLASS := ../../../device/samsung/ha3g/ril
 
@@ -73,9 +71,6 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 # SELinux
 BOARD_SEPOLICY_DIRS += device/samsung/exynos5420-common/sepolicy
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
-
-# Microphone recording fix.- REQUIRES PATCH
-BOARD_USES_LEGACY_AUDIO_BLOB := true
 
 # Inherit from exynos5420-common
 include device/samsung/exynos5420-common/BoardConfigCommon.mk
