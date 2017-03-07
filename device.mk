3cd6dfe59a0770c8f0af018be9cc58fe5f6cccc0
Martin Bouchet <tincho5588@gmail.com>
2017-03-07 08:50:45 -0300

    camera: build Snap
    Change-Id: Ie23f9e7e596b749cc7ccc5505401c58faaec86f2

--- device.mk
+++ device.mk
@@ -26,6 +26,10 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     libtinyalsa
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/ysound.xml:system/etc/ysound.xml
