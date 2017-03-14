984bb6be8ef80571ec8852f976a25236917cd768
mosimchah <mosimchah@gmail.com>
2017-03-14 10:25:17 -0400

    optimize everything for LD
    Change-Id: Ib47b54b04c6fa23fc5505b8327d2c5742b807102

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 TARGET_OTA_ASSERT_DEVICE := ha3g
 
 # Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_deathly_ha3g_defconfig
+TARGET_KERNEL_CONFIG := liquiddeathos_deathly_ha3g_defconfig
 
 # IR Blaster
 IR_HAS_ONE_FREQ_RANGE := true
