d0f09a9014242165829257d11094fec0db96215c
Martin Bouchet <tincho5588@gmail.com>
2017-07-21 00:42:48 -0400

    ril: refactor ril headers
    Change-Id: I109ef482e087a7c0289d01a2bee58d5045896693

--- include/telephony/ril_unsol_commands_vendor.h
+++ include/telephony/ril_unsol_commands_vendor.h
@@ -1,4 +1,4 @@
-/* //device/libs/telephony/ril_unsol_commands.h
+/* // device/libs/telephony/ril_unsol_commands.h
 **
 ** Copyright 2006, The Android Open Source Project
 **
@@ -6,7 +6,7 @@
 ** you may not use this file except in compliance with the License.
 ** You may obtain a copy of the License at
 **
-**     http://www.apache.org/licenses/LICENSE-2.0
+**     http:// www.apache.org/licenses/LICENSE-2.0
 **
 ** Unless required by applicable law or agreed to in writing, software
 ** distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,40 +14,39 @@
 ** See the License for the specific language governing permissions and
 ** limitations under the License.
 */
-    {RIL_OEM_UNSOL_RESPONSE_BASE, NULL, WAKE_PARTIAL},
-    {RIL_UNSOL_RESPONSE_NEW_CB_MSG, responseVoid, WAKE_PARTIAL}, // 11000 X
+    {SAMSUNG_UNSOL_RESPONSE_BASE, NULL, WAKE_PARTIAL}, // 11000
     {RIL_UNSOL_RELEASE_COMPLETE_MESSAGE, responseVoid, WAKE_PARTIAL}, // 11001
     {RIL_UNSOL_STK_SEND_SMS_RESULT, responseInts, WAKE_PARTIAL}, // 11002
-    {RIL_UNSOL_STK_CALL_CONTROL_RESULT, responseVoid, WAKE_PARTIAL}, // 11003
+    {RIL_UNSOL_STK_CALL_CONTROL_RESULT, responseString, WAKE_PARTIAL}, // 11003
     {11004, NULL, WAKE_PARTIAL}, // 11004
-    {11005, NULL, WAKE_PARTIAL}, //11005
-    {11006, NULL, WAKE_PARTIAL},
+    {11005, NULL, WAKE_PARTIAL}, // 11005
+    {11006, NULL, WAKE_PARTIAL}, // 11006
     {11007, NULL, WAKE_PARTIAL}, // 11007
     {RIL_UNSOL_DEVICE_READY_NOTI, responseVoid, WAKE_PARTIAL}, // 11008
     {RIL_UNSOL_GPS_NOTI, responseVoid, WAKE_PARTIAL}, // 11009
     {RIL_UNSOL_AM, responseString, WAKE_PARTIAL}, // 11010
-    {110011, NULL, WAKE_PARTIAL}, // 11011
-    {110012, NULL, WAKE_PARTIAL}, // 11012
-    {RIL_UNSOL_SAP, responseVoid, WAKE_PARTIAL}, // 11013
+    {RIL_UNSOL_DUN_PIN_CONTROL_SIGNAL, responseVoid, WAKE_PARTIAL}, // 11011
+    {RIL_UNSOL_DATA_SUSPEND_RESUME, responseInts, WAKE_PARTIAL}, // 11012
+    {RIL_UNSOL_SAP, responseRaw, WAKE_PARTIAL}, // 11013
     {11014, NULL, WAKE_PARTIAL},
     {11015, NULL, WAKE_PARTIAL}, // 11015
     {11016, NULL, WAKE_PARTIAL}, // 11016
     {RIL_UNSOL_WB_AMR_STATE, responseInts, WAKE_PARTIAL}, // 11017
     {11018, NULL, WAKE_PARTIAL}, // 11018
     {11019, NULL, WAKE_PARTIAL}, // 11019
-    {RIL_UNSOL_UART, responseVoid, WAKE_PARTIAL}, // 11020
-    {RIL_UNSOL_SIM_PB_READY, responseVoid, WAKE_PARTIAL}, // 11021 X
+    {RIL_UNSOL_UART, responseRaw, WAKE_PARTIAL}, // 11020
+    {RIL_UNSOL_SIM_PB_READY, responseVoid, WAKE_PARTIAL}, // 11021
     {11022, NULL, WAKE_PARTIAL}, // 11022
     {11023, NULL, WAKE_PARTIAL}, // 11023
-    {RIL_UNSOL_VE, responseVoid, WAKE_PARTIAL}, // 11024 X
+    {RIL_UNSOL_VE, responseRaw, WAKE_PARTIAL}, // 11024
     {11025, NULL, WAKE_PARTIAL}, // 11025
-    {RIL_UNSOL_FACTORY_AM, responseVoid, WAKE_PARTIAL}, // 11026 X
-    {RIL_UNSOL_IMS_REGISTRATION_STATE_CHANGED, responseVoid, WAKE_PARTIAL}, // 11027 X
-    {RIL_UNSOL_MODIFY_CALL, responseVoid, WAKE_PARTIAL}, // 11028 X
-    {RIL_UNSOL_SRVCC_HANDOVER, responseVoid, WAKE_PARTIAL}, // 11029 X
-    {RIL_UNSOL_CS_FALLBACK, responseVoid, WAKE_PARTIAL}, // 11030 X
+    {RIL_UNSOL_FACTORY_AM, responseVoid, WAKE_PARTIAL}, // 11026
+    {RIL_UNSOL_IMS_REGISTRATION_STATE_CHANGED, responseInts, WAKE_PARTIAL}, // 11027
+    {RIL_UNSOL_MODIFY_CALL, responseVoid, WAKE_PARTIAL}, // 11028
+    {RIL_UNSOL_SRVCC_HANDOVER, responseVoid, WAKE_PARTIAL}, // 11029
+    {RIL_UNSOL_CS_FALLBACK, responseInts, WAKE_PARTIAL}, // 11030
     {11031, NULL, WAKE_PARTIAL}, // 11031
-    {RIL_UNSOL_VOICE_SYSTEM_ID, responseVoid, WAKE_PARTIAL}, // 11032 X
+    {RIL_UNSOL_VOICE_SYSTEM_ID, responseInts, WAKE_PARTIAL}, // 11032
     {11033, NULL, WAKE_PARTIAL}, // 11033
     {RIL_UNSOL_IMS_RETRYOVER, responseVoid, WAKE_PARTIAL}, // 11034
     {RIL_UNSOL_PB_INIT_COMPLETE, responseVoid, WAKE_PARTIAL}, // 11035
@@ -69,17 +68,16 @@
     {11051, NULL, WAKE_PARTIAL}, // 11051
     {11052, NULL, WAKE_PARTIAL}, // 11052
     {11053, NULL, WAKE_PARTIAL}, // 11053
-    {RIL_UNSOL_STK_CALL_STATUS, responseVoid, WAKE_PARTIAL}, // 11054
-    {RIL_UNSOL_ON_SS, responseVoid, WAKE_PARTIAL}, // 11055
-    {11056, NULL, WAKE_PARTIAL}, // 11056
-    {11057, NULL, WAKE_PARTIAL}, // 11057
-    {11058, NULL, WAKE_PARTIAL}, // 11058
+    {RIL_UNSOL_STK_CALL_STATUS, responseInts, WAKE_PARTIAL}, // 11054
+    {11055, NULL, WAKE_PARTIAL}, // 11055
+    {RIL_UNSOL_MODEM_CAP, responseRaw, WAKE_PARTIAL}, // 11056
+    {RIL_UNSOL_SIM_SWAP_STATE_CHANGED, responseInts, WAKE_PARTIAL}, // 11057
+    {RIL_UNSOL_SIM_COUNT_MISMATCHED, responseInts, WAKE_PARTIAL}, // 11058
     {11059, NULL, WAKE_PARTIAL}, // 11059
-    {11060, NULL, WAKE_PARTIAL}, // 11060
-    {RIL_UNSOL_IMS_PREFERENCE_CHANGED, responseVoid, WAKE_PARTIAL}, // 11061
-#ifdef RIL_UNSOL_SNDMGR_WB_AMR_REPORT
-    {RIL_UNSOL_SNDMGR_WB_AMR_REPORT, responseInts, WAKE_PARTIAL}, // 20017
-#endif
-#ifdef RIL_UNSOL_SNDMGR_CLOCK_CTRL
-    {RIL_UNSOL_SNDMGR_CLOCK_CTRL, responseInts, WAKE_PARTIAL}, // 20022
-#endif
+    {RIL_UNSOL_DUN, responseString, WAKE_PARTIAL}, // 11060
+    {RIL_UNSOL_IMS_PREFERENCE_CHANGED, responseInts, WAKE_PARTIAL}, // 11061
+    {RIL_UNSOL_SIM_APPLICATION_REFRESH, responseInts, WAKE_PARTIAL}, // 11062
+    {RIL_UNSOL_UICC_APPLICATION_STATUS, responseVoid, WAKE_PARTIAL}, // 11063
+    {RIL_UNSOL_VOICE_RADIO_BEARER_HO_STATUS, responseInts, WAKE_PARTIAL}, // 11064
+    {RIL_UNSOL_CLM_NOTI, responseVoid, WAKE_PARTIAL}, // 11065
+    {RIL_UNSOL_SIM_ICCID_NOTI, responseString, WAKE_PARTIAL}, // 11066
