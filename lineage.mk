1bf64062ab58319db79b2db5f36956048ef2d53d
Martin Bouchet <tincho5588@gmail.com>
2017-09-05 04:00:39 -0300

    ha3g: do not include missing makefiles
    Change-Id: I8a821f95d9899deb8d44dd80f38ae75e765a2f4e

--- lineage.mk
+++ lineage.mk
@@ -1,15 +1,14 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 
 # Inherit from ha3g device
 $(call inherit-product, device/samsung/ha3g/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit more LineageOS stuff.
-$(call inherit-product, vendor/cm/config/telephony.mk)
+$(call inherit-product, vendor/lineage/config/telephony.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_ha3g
